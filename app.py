ac69f0142a267d53e571e4588853f16b476dbdf8
Vivi <vvw224@gmail.com>
2019-09-05 09:36:40 +0800

    fix app,py

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ from linebot.models import (
 app = Flask(__name__)
 
 
-# wb ↓
-# get channel_secret and channel_access_token from your enviroment variable
-channel_secret = os.getenv('a1bc96cb9e5e1dba49135aa3acb73803', None)
-channel_access_token = os.getenv('P45kdvqNpZchU8nBey8rSHxP7HccFgH43bsRzv+CQ1vPGoC72d3C51XkDFUIbToaYc+pvURu7u/6YHR1mogK3i0ZSsMm6fXJgJFQJYee6RS0tXexfuZ2yOFO7QlQCffIVU0YUa3vD9KeH7m51UO5oAdB04t89/1O/w1cDnyilFU=', None)
-assert channel_secret is not None, 'Specify a1bc96cb9e5e1dba49135aa3acb73803 as enviroment variable.'
-assert channel_access_token is not None, 'Specify P45kdvqNpZchU8nBey8rSHxP7HccFgH43bsRzv+CQ1vPGoC72d3C51XkDFUIbToaYc+pvURu7u/6YHR1mogK3i0ZSsMm6fXJgJFQJYee6RS0tXexfuZ2yOFO7QlQCffIVU0YUa3vD9KeH7m51UO5oAdB04t89/1O/w1cDnyilFU= as enviroment variabel.'
-# wb ↑
+# # wb ↓
+# # get channel_secret and channel_access_token from your enviroment variable
+# channel_secret = os.getenv('a1bc96cb9e5e1dba49135aa3acb73803', None)
+# channel_access_token = os.getenv('P45kdvqNpZchU8nBey8rSHxP7HccFgH43bsRzv+CQ1vPGoC72d3C51XkDFUIbToaYc+pvURu7u/6YHR1mogK3i0ZSsMm6fXJgJFQJYee6RS0tXexfuZ2yOFO7QlQCffIVU0YUa3vD9KeH7m51UO5oAdB04t89/1O/w1cDnyilFU=', None)
+# assert channel_secret is not None, 'Specify a1bc96cb9e5e1dba49135aa3acb73803 as enviroment variable.'
+# assert channel_access_token is not None, 'Specify P45kdvqNpZchU8nBey8rSHxP7HccFgH43bsRzv+CQ1vPGoC72d3C51XkDFUIbToaYc+pvURu7u/6YHR1mogK3i0ZSsMm6fXJgJFQJYee6RS0tXexfuZ2yOFO7QlQCffIVU0YUa3vD9KeH7m51UO5oAdB04t89/1O/w1cDnyilFU= as enviroment variabel.'
+# # wb ↑
 
 line_bot_api = LineBotApi('P45kdvqNpZchU8nBey8rSHxP7HccFgH43bsRzv+CQ1vPGoC72d3C51XkDFUIbToaYc+pvURu7u/6YHR1mogK3i0ZSsMm6fXJgJFQJYee6RS0tXexfuZ2yOFO7QlQCffIVU0YUa3vD9KeH7m51UO5oAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('a1bc96cb9e5e1dba49135aa3acb73803')
